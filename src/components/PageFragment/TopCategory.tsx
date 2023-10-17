6789f91e3d2d520da3aa8410cc6b5a4567dc29f7
giddyPwild <gadegunle198@stu.ui.edu.ng>
2023-10-17 16:36:21 +0100

    second commit

--- src/components/PageFragment/TopCategory.tsx
+++ src/components/PageFragment/TopCategory.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { SiAirplayaudio } from "react-icons/si";
+// import { SiAirplayaudio } from "react-icons/si";
 import TopCatCard from "../reusable/Cards/TopCatCard";
 
 const TopCategory = () => {
@@ -7,7 +7,7 @@ const TopCategory = () => {
 		<div className='bg-[#30303080] mt-6 w-full pb-4'>
 			<div className='flex items-center gap-1 pt-5'>
 				<div className=''>
-					<SiAirplayaudio size={15} color='#7B61FF' />
+					{/* <SiAirplayaudio size={15} color='#7B61FF' /> */}
 				</div>
 				<h4 className='text-white font-extrabold leading-6 py-0'>
 					Top categories
@@ -16,7 +16,7 @@ const TopCategory = () => {
 			<div className='flex w-full'>
 				<div className='mt-4 flex gap-4 flex-wrap flex-[.8]'>
 					<TopCatCard
-						icon={<SiAirplayaudio size={15} />}
+					
 						bgColor='#575757'
 						text='Comedy'
 					/>
