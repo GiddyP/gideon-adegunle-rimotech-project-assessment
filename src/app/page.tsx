a5f28468b6f9776f3f451599b3f049115d5a4900
giddyPwild <gadegunle198@stu.ui.edu.ng>
2023-10-17 16:30:52 +0100

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import AppLayout from "@src/components/AppLayout";
-
+import DiscoverOtherListeners from "@src/components/PageFragment/DiscoverOtherListeners";
+import PickOfTheWeek from "@src/components/PageFragment/PickOfTheWeek";
+import PodcastHostSuggestions from "@src/components/PageFragment/PodcastHostSuggestions";
+import PopularAndTrending from "@src/components/PageFragment/PopularAndTrending";
+import SearchByKeyword from "@src/components/PageFragment/SearchByKeyword";
+import TopCategory from "@src/components/PageFragment/TopCategory";
+import Header from "@src/components/PageFragment/Header";
+import Footer from "../components/PageFragment/Footer";
 const page = () => {
 	return (
 		<AppLayout>
-			{/* <div className='bg-black-400 mb-72'>
+			<div className='bg-black-400 mb-72'>
 				<Header />
 				<PickOfTheWeek />
 				<TopCategory />
@@ -14,7 +21,7 @@ const page = () => {
 				<DiscoverOtherListeners />
 				<SearchByKeyword />
 				
-			</div> */}
+			</div>
 		</AppLayout>
 	);
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,64 +1,41 @@
-/** @type {import('tailwindcss').Config} */
-
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  darkMode: 'class',
-  theme: {
-    extend: {
-      fontFamily: {
-        // mont: ['var(--font-mont)'],
-        poppins: ['var(--font-poppins)'],
-      },
-      colors: {
-        dark: "#1b1b1b",
-        light: "#f5f5f5",
-        primary: "#e99d2c", // 240,86,199
-        primaryDark: "#58E6D9", // 80,230,217
-      },
-      animation: {
-        'spin-slow': 'spin 8s linear infinite',
-      },
-      backgroundImage: {
-        circularLight:
-          "repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f5f5f5 5px,#f5f5f5 100px)",
-
-        circularDark:
-          "repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#1b1b1b 8px,#1b1b1b 100px)",
-
-        circularLightLg:
-          "repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f5f5f5 5px,#f5f5f5 80px)",
-
-        circularDarkLg:
-          "repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#1b1b1b 8px,#1b1b1b 80px)",
-
-        circularLightMd:
-          "repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f5f5f5 5px,#f5f5f5 60px)",
-
-        circularDarkMd:
-          "repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#1b1b1b 6px,#1b1b1b 60px)",
-
-        circularLightSm:
-          "repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f5f5f5 5px,#f5f5f5 40px)",
-
-        circularDarkSm:
-          "repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#1b1b1b 4px,#1b1b1b 40px)",
-      }
-    },
-    screens: {
-      "2xl": { max: "1535px" }, // => @media (max-width: 1535px) { ... }
-      xl: { max: "1279px" }, // => @media (max-width: 1279px) { ... }
-      lg: { max: "1023px" }, // => @media (max-width: 1023px) { ... }
-      slg: { max: "999px" }, // => @media (max-width: 1023px) { ... }
-      xmd: { max: "874px" }, // => @media (max-width: 767px) { ... }
-      md: { max: "767px" }, // => @media (max-width: 767px) { ... }
-      sm: { max: "639px" },// => @media (max-width: 639px) { ... }
-      xs: { max: "479px" },// => @media (max-width: 479px) { ... }}
-      xss: { max: "476px" },// => @media (max-width: 479px) { ... }}
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	darkMode: "class",
+	theme: {
+		extend: {
+			fontFamily: {
+				poppins: ["var(--font-poppins)", "sans-serif"],
+			},
+			colors: {
+				light: "#F3F3F3",
+				gray: "#F9F5FA",
+				primary: "#A97CB8",
+				black: {
+					100: "#000",
+					200: "#303030",
+					300: "#3A3A3A",
+					400: "#272727",
+				},
+				secondary: {
+					200: "#F2ECF5",
+				},
+        orange: "#FB5646",
+        lemon: "#1EAEA3"
+			},
+			animation: {
+				"spin-slow": "spin 8s linear infinite",
+			},
+		},
+		screens: {
+			xs: "400px",
+			slg: "999px", // @media (min-width: 999px)
+			xmd: "800px", // @media (min-width: 800px)
+			...require("tailwindcss/defaultTheme").screens,
+		},
+	},
+	plugins: [],
 };
