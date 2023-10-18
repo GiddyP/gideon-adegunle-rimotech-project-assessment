5af025c359cd342ec8e72f0a031561bb3c7b2188
giddyPwild <gadegunle198@stu.ui.edu.ng>
2023-10-18 07:47:09 +0100

    resolved bug

--- src/components/PageFragment/Header.tsx
+++ src/components/PageFragment/Header.tsx
@@ -10,6 +10,7 @@ import { SignupImage } from "@public/images";
 
 const Header = () => {
 	const [isModalSignUpOpen, setIsModalSignUpOpen] = useState(false);
+	const [isModalLoginOpen, setIsModalLoginOpen] = useState(false);
 
 	const openSignUpModal = () => {
 		setIsModalSignUpOpen(true);
@@ -18,9 +19,19 @@ const Header = () => {
 	const closeSignUpModal = () => {
 		setIsModalSignUpOpen(false);
 	};
+	const openLoginModal = () => {
+		setIsModalLoginOpen(true);
+	};
+
+	const closeLoginModal = () => {
+		setIsModalLoginOpen(false);
+	};
 
 	const handleSignUp = () => {
 		closeSignUpModal();
+	};
+	const handleLogin = () => {
+		closeLoginModal();
 	};
 
 	const SignupContent = () => {
@@ -67,7 +78,46 @@ const Header = () => {
 						))}
 					</ul>
 					<h4 className='text-base text-[#CDCDCD] leading-8'>
-						Already have an account? <span>Log in</span>
+						Already have an account?{" "}
+						<span
+							onClick={openLoginModal}
+							className='hover:underline hover:text-lemon transition cursor-pointer'
+						>
+							Log in
+						</span>
+					</h4>
+				</div>
+				<div className='w-1/2 flex flex-col items-center'>
+					<img src={SignupImage.src} alt={SignupImage.alt} />
+					<h4 className='text-[#7B61FF] text-2xl leading-8 text-center font-extrabold'>
+						Join a community of podcast lovers and creators
+					</h4>
+				</div>
+			</div>
+		);
+	};
+	const LoginContent = () => {
+		return (
+			<div className='flex w-full justify-center gap-5 px-10'>
+				<div className='flex w-1/2 flex-col items-center gap-3'>
+					<h4 className='text-xl font-extrabold text-white'>
+						Log in with your socials below
+					</h4>
+					<ul className='flex flex-col gap-4'>
+						<li className='bg-white rounded-[32px] flex justify-between items-center px-9 w-[351px] h-[59px]'>
+							<span className='bg-[#EDE6E5] p-2 rounded-full'>
+								<RiGoogleLine size={23} color='#282828' />
+							</span>
+							<span className='flex-[.8] text-base text-[#282828] font-semibold'>
+								Choose account
+							</span>
+							<span>
+								<AiOutlineRight color='#282828' />
+							</span>
+						</li>
+					</ul>
+					<h4 className='text-base font-medium text-white'>
+						Log in with email.
 					</h4>
 				</div>
 				<div className='w-1/2 flex flex-col items-center'>
@@ -106,7 +156,10 @@ const Header = () => {
 					<AiOutlineAudio color='#fff' />
 					<span className='text-white'>Sign Up</span>
 				</button>
-				<button className='flex gap-2 items-center bg-black-100 hover:bg-black-200 transition shadow-md-[#998] px-4 py-2 rounded-lg'>
+				<button
+					onClick={openLoginModal}
+					className='flex gap-2 items-center bg-black-100 hover:bg-black-200 transition shadow-md-[#998] px-4 py-2 rounded-lg'
+				>
 					<BiUser color='#fff' />
 					<span className='text-white'>Log In</span>
 				</button>
@@ -116,6 +169,13 @@ const Header = () => {
 					content={<SignupContent />}
 					buttonText='Sign Up'
 					setIsOpen={setIsModalSignUpOpen}
+				/>
+				<SignupModal
+					isOpen={isModalLoginOpen}
+					onClose={LoginContent}
+					content={<LoginContent />}
+					buttonText='Login'
+					setIsOpen={setIsModalLoginOpen}
 				/>
 			</div>
 		</header>
